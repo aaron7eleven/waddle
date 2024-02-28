37ac1b13cd34cf6ec3630a1d519c5783bd3286d5
aaron7eleven <aatartanfb@gmail.com>
2024-02-27 18:32:36 -0800

    not worring about logging right now

--- include/util/waddle_log.h
+++ include/util/waddle_log.h
@@ -1,30 +1,30 @@
-#pragma once
-
-#ifdef WADDLE_EXPORTS
-#define WADDLE_API __declspec(dllexport)
-#else
-#define WADDLE_API __declspec(dllimport)
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <stdio.h>
-
-typedef enum {
-	LOG,
-	WARNING,
-	ERROR,
-	FATAL
-} waddle_log_level;
-
-WADDLE_API void waddle_log_by_level(const char* msg, waddle_log_level log_level);
-WADDLE_API void waddle_log(const char* msg);
-WADDLE_API void waddle_log_warning(const char* msg);
-WADDLE_API void waddle_log_error(const char* msg);
-WADDLE_API void waddle_log_fatal(const char* msg);
-
-#ifdef __cplusplus
-}
-#endif
+//#pragma once
+//
+//#ifdef WADDLE_EXPORTS
+//#define WADDLE_API __declspec(dllexport)
+//#else
+//#define WADDLE_API __declspec(dllimport)
+//#endif
+//
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
+//
+//#include <stdio.h>
+//
+//typedef enum {
+//	LOG,
+//	WARNING,
+//	ERROR,
+//	FATAL
+//} waddle_log_level;
+//
+//WADDLE_API void waddle_log_by_level(waddle_log_level log_level, const char* format, ...);
+//WADDLE_API void waddle_log(const char* msg);
+//WADDLE_API void waddle_log_warning(const char* msg);
+//WADDLE_API void waddle_log_error(const char* msg);
+//WADDLE_API void waddle_log_fatal(const char* msg);
+//
+//#ifdef __cplusplus
+//}
+//#endif
--- src/util/waddle_log.c
+++ src/util/waddle_log.c
@@ -1,46 +1,49 @@
-#include "util/waddle_log.h"
-#include "util/waddle_string.h"
-
-void waddle_log(const char* msg,...) {
-	printf(msg);
-}
-
-WADDLE_API void waddle_log_by_level(const char* msg, waddle_log_level log_level) {
-	switch (log_level)
-	{
-	case LOG: {
-		printf("%s", waddle_strcat("WADDLE_LOG: ", msg));
-	} break;
-
-	case WARNING: {
-		printf("%s", waddle_strcat("WADDLE_WARNING: ", msg));
-	} break;
-
-	case ERROR: {
-		printf("%s", waddle_strcat("WADDLE_ERROR: ", msg));
-	} break;
-
-	case FATAL: {
-		printf("%s", waddle_strcat("WADDLE_FATAL: ", msg));
-	} break;
-
-	default:
-		break;
-	}
-}
-
-WADDLE_API void waddle_log(const char* msg) {
-	waddle_log_by_level(msg, LOG);
-}
-
-WADDLE_API void waddle_log_warning(const char* msg) {
-	waddle_log_by_level(msg, WARNING);
-}
-
-WADDLE_API void waddle_log_error(const char* msg) {
-	waddle_log_by_level(msg, ERROR);
-}
-
-WADDLE_API void waddle_log_fatal(const char* msg) {
-	waddle_log_by_level(msg, FATAL);
-}
+//#include <stdarg.h>
+//#include "util/waddle_log.h"
+//#include "util/waddle_string.h"
+//
+//WADDLE_API void waddle_log_by_level(waddle_log_level log_level, const char* format, ...) {
+//	va_list args;
+//	va_start(args, format);
+//	
+//	switch (log_level)
+//	{
+//	case LOG: {
+//		vprintf("%s", waddle_strcat("WADDLE_LOG: ", format));
+//	} break;
+//
+//	case WARNING: {
+//		vprintf("%s", waddle_strcat("WADDLE_WARNING: ", format));
+//	} break;
+//
+//	case ERROR: {
+//		vprintf("%s", waddle_strcat("WADDLE_ERROR: ", format));
+//	} break;
+//
+//	case FATAL: {
+//		vprintf("%s", waddle_strcat("WADDLE_FATAL: ", format));
+//	} break;
+//
+//	default:
+//		vprintf("WADDLE_ERROR: Unknown log level in waddle_log_by_level");
+//		break;
+//	}
+//
+//	va_end(args);
+//}
+//
+//WADDLE_API void waddle_log(const char* msg, ...) {
+//	waddle_log_by_level(LOG, msg, ...);
+//}
+//
+//WADDLE_API void waddle_log_warning(const char* msg) {
+//	waddle_log_by_level(msg, WARNING);
+//}
+//
+//WADDLE_API void waddle_log_error(const char* msg) {
+//	waddle_log_by_level(msg, ERROR);
+//}
+//
+//WADDLE_API void waddle_log_fatal(const char* msg) {
+//	waddle_log_by_level(msg, FATAL);
+//}
